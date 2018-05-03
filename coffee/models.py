f21644d79908ded1bd6acac0fe017ea0703b9d3e
Grace Medeiros <medegr@student.breckschool.org>
2018-05-03 13:11:08 -0500

    changed
    changed

--- coffee/models.py
+++ coffee/models.py
@@ -14,6 +14,20 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        #string representation of our object
+        
         return self.title
-# Create your models here.
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField(max_length = 200)
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank = True, null = True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- coffee/views.py
+++ coffee/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
-
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -42,4 +41,15 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-# Create your views here.
+ def post_comment(request):
+     if request.method == "POST":
+         form = PostForm(request.POST)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm()
+     return render(request, 'blog/post_edit.html', {'form': form})
